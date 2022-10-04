eb00bf62ec871e3db9a5ae8fdfd534532414cef8
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-10-04 14:37:53 -0700

    added readme

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const notes = require("./db/db.json");
+const notesDB = require("./db/db.json");
 const uuid = require("uuid");
 const application = express();
 const PORT = process.env.PORT || 3002;
@@ -10,21 +10,22 @@ const { DH_CHECK_P_NOT_SAFE_PRIME, SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require("
 
 application.use(express.urlencoded({ extended: true}));
 application.use(express.json());
-application.use(express.static("./public"));
+application.use(express.static("public"));
 
 application.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./db/db.json"))});
+    res.sendFile(path.join(__dirname, "./public/notes.html"))});
 
 application.post("/api/notes", (req, res) => {
     const notes = JSON.parse(fs.readFileSync("./db/db.json"));
     const newNote = req.body;
+    console.log(newNote);
     newNote.id = uuid.v4();
     notes.push(newNote);
     fs.writeFileSync("./db/db.json", JSON.stringify(notes))
     res.json(notes);
 });
 
-application.get("/", function (req, res) {
+application.get("/", function (req, res) {ß
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 application.get("/notes", function (req, res) {
